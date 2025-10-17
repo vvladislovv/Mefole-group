c669f0ca3ee2e193f3ce3a9e569ed532811c9346
vvladislovv <vlad.yelcheninov@gmail.com>
2025-10-17 16:53:17 +0300

    отзывы готовы

--- frontend/src/data/reviews.js
+++ frontend/src/data/reviews.js
@@ -4,9 +4,14 @@ export const reviews = [
   { id: "3", rating: 5 },
   { id: "4", rating: 4 },
   { id: "5", rating: 4 },
-  { id: "6", rating: 4 },
-  { id: "7", rating: 5 },
-  { id: "8", rating: 4 },
-  { id: "9", rating: 5 },
-  { id: "10", rating: 5 }
+  { id: "6", rating: 5 },
+  { id: "7", rating: 4 },
+  { id: "8", rating: 5 },
+  { id: "9", rating: 4 },
+  { id: "10", rating: 4 },
+  { id: "11", rating: 5 },
+  { id: "12", rating: 4 },
+  { id: "13", rating: 4 },
+  { id: "14", rating: 5 },
+  { id: "15", rating: 4 }
 ];
